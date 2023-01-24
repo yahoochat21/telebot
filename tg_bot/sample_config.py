4dcb6dccad1f12c12e22ef4a9fa5037acd92c66c
yahoochat21 <122388329+yahoochat21@users.noreply.github.com>
2023-01-24 06:32:50 +0545

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -10,12 +10,12 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = ""
+    API_KEY = "5810794089:AAF3JxZc3mJv5TalLkL-_G5IcCW7CyhIcmk"
     OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
     OWNER_USERNAME = "ViruZs"
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    SQLALCHEMY_DATABASE_URI = 'postgres://yahoo:Nepal@ktm1@143.244.130.145:5432/telebot'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
     LOAD = []
     NO_LOAD = ['translation', 'rss']
